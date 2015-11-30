780532643de31c4197a47de22b85f21bea3c2aaa
Catherine White <ccavigl1@jhu.edu>
2015-11-30 14:11:06 -0500

    trying to get automodule working on readthedocs still

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #My attempt at a setup... I have no idea how this is supposed to work.
 
-from distutils.core import setup
-
-setup(name='Py2PAC',
-      version='0.1',
-      py_modules=['numpy', 'AngularCatalog_class'],
-      )
+ffrom setuptools import setup, find_packages
+setup(
+    name = "Py2PAC",
+    version = "0.1",
+    packages = find_packages(),
+)
