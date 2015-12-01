1b098f499bfd37d101b95c9163f2720244d2da61
Catherine White <ccavigl1@jhu.edu>
2015-11-30 21:56:42 -0500

    trying to get automodule working on readthedocs still

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
         
-MOCK_MODULES = ['sklearn', 'sklearn.neighbors']
+MOCK_MODULES = ['astroML']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
         
 def readme():
