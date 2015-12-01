011f14d23dd4a742ae2832f86079ced8f74d54f6
Catherine White <ccavigl1@jhu.edu>
2015-11-30 21:24:49 -0500

    trying to get automodule working on readthedocs still

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.abspath('numpydoc'))
+sys.path.insert(0, os.path.abspath('..'))
 
 #Numpydoc options
 numpydoc_show_class_members = True
