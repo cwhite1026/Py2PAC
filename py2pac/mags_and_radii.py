f3e5467bda703b125f2f760dfa95526959b10f20
Meredith Durbin <mdurbin@stsci.edu>
2016-04-20 15:35:59 -0400

    LOGS

--- py2pac/mags_and_radii.py
+++ py2pac/mags_and_radii.py
@@ -41,6 +41,11 @@ def get_mags_and_radii(size, min_mag = 20, max_mag = 28, z = 1.25, mstar = 9.5):
 
     z : scalar, optional
         Redshift of fake galaxies
+        Allowed values: 1.25, 2, 3.5, 5.25
+
+    mstar : scalar, optional
+        Log of stellar mass of fake galaxies
+        Allowed values: 8.5, 9.5, 11
 
     Returns
     ----------
@@ -50,11 +55,6 @@ def get_mags_and_radii(size, min_mag = 20, max_mag = 28, z = 1.25, mstar = 9.5):
             Array of log of radii in pixels for randoms
     """
     df = default_cosmology.get_cosmology_from_string('Planck13')
-    #loc, scale = -0.398557693623, 1.74601245963
-    # z_array = np.random.normal(shape, loc, scale, size=size)
-    # z_array[z_array < 0.4] = z
-    # z_array[z_array > z] = z
-    # dmod_array = df.distmod(z_array).value
     dmod = df.distmod(z).value
     mags = get_gaussian_mags(size, z, mstar)
     absolute_mags = mags - dmod
@@ -96,75 +96,12 @@ def get_radii(m, r0 = 0.21 / 0.06, m0 = -21., beta = 0.3, sigma = 0.7):
         log_rand : array
             Array of log of radii in pixels
     """
-    exp = 10**(0.4*(m0 - m))
+    exp = 10**(-0.4*(m - m0))
     mean = r0 * exp**beta
-    rand = np.random.lognormal(mean, sigma)
+    rand = np.random.lognormal(np.log(mean), sigma)
     log_rand = np.log10(rand)
     if type(log_rand).__name__ != 'ndarray':
         log_rand = np.array([log_rand])
     log_rand[log_rand > 2.5] = 2.5
     log_rand[log_rand < -1.] = -1.
     return log_rand
-
-def schechter(m, phi_star=0.00681, m_star=-19.61, alpha=-1.33):
-    """
-    Returns number densities of magnitudes according to schechter function
-    Default parameters for z=1.7 from here:
-    http://mnras.oxfordjournals.org/content/456/3/3194.full.pdf
-
-    Parameters
-    ----------
-    m : float or array
-        Input magnitudes
-        
-    phi_star : scalar, optional
-        Default is 0.21 / 0.06 (arcsec / GOODS pixel scale)
-
-    m_star : scalar, optional
-        Default is -19.61
-
-    alpha : scalar, optional
-        Faint-end slope of Schechter function
-
-    Returns
-    ----------
-        n : float or array
-            Number density of galaxies by magnitude
-    """
-    fudge_factor = np.log(10)*0.4 * phi_star
-    base_exp = 10**(0.4*(m_star - m))
-    n = fudge_factor * (base_exp**(1 + alpha)) * np.exp(-base_exp)
-    return n
-
-def get_schechter_mags(size, distmod, min_mag, max_mag):
-    """
-    Cuts off sample of random magnitudes according to schechter function
-
-    Parameters
-    ----------
-    size : int
-        Number of magnitudes to generate
-        
-    distmod : scalar
-        Distance modulus for conversion between absolute and apparent mags
-
-    min_mag : scalar
-        Numerically lowest (brightest) allowable magnitude
-
-    max_mag : scalar
-        Numerically highest (dimmest) allowable magnitude
-
-    Returns
-    ----------
-        mags : array
-            Array of absolute magnitudes for randoms
-            sampled according to Schechter function
-    """
-    nmax = schechter(max_mag - distmod)
-    mags = []
-    while len(mags) < size:
-        m = np.random.uniform(min_mag, max_mag, size=size) - distmod
-        n = np.random.uniform(0, high=nmax, size=size)
-        mags += list(m[n <= schechter(m)])   
-    mags = np.asarray(mags[:size])
-    return mags
