95c6f426b2517a2722b2e7d24cdf54d80ec9902b
Catherine White <ccavigl1@jh.edu>
2016-05-12 08:24:58 -0400

    diagnosing completeness function problem 2

--- py2pac/ImageMask_class.py
+++ py2pac/ImageMask_class.py
@@ -1359,6 +1359,7 @@ class ImageMask:
                                 if rad_list is not None:
                                     rads_in_ranges = rad_list[in_ranges]
                                     rads = rads_in_ranges[at_level]
+                                print mags, rads
                                 temp_complete[at_level] = cf.query(mags, 
                                                                 r_list=rads)
             else:
